50f544df262faf213885140f66d3012e2ef35c65
PHeonix25 <p@hermens.com.au>
2013-06-07 20:00:11 +1000

    Updated to fix some typos Updated to use LINQ Updated to use case & culture insensitive comparisons Updated to be polite when exiting for the user Updated to use system-based line-endings

--- WifiProfiles/Program.cs
+++ WifiProfiles/Program.cs
@@ -1,26 +1,35 @@
 ﻿using NetSh;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WifiProfiles
 {
     class Program
     {
         static void Main(string[] args)
-        { 
-            //command line arguments officially got irritating at this point but I don't feel like bringing in a whole library.
-            if (args.Length > 1 && args[0].ToUpperInvariant() == "DELETE" && !string.IsNullOrEmpty(args[1]))
+        {
+            if (args.Any() && String.Equals(args.First(), "DELETE", StringComparison.CurrentCultureIgnoreCase)) // You're using LINQ anyway?
                 Delete(args[1]);
             else
-                List(args.Length == 1 && args[0].ToUpperInvariant() == "/DELETEAUTOOPEN");
+                List(args.Length == 1 && String.Equals(args.First(), "/DELETEAUTOOPEN", StringComparison.CurrentCultureIgnoreCase));
         }
 
         static void Delete(string profileName)
         {
             Console.WriteLine(NetShWrapper.DeleteWifiProfile(profileName));
+        }
+
+        static void Pause()
+        {
+            Console.WriteLine();
+            var pauseProc = Process.Start(new ProcessStartInfo()
+                {
+                    FileName = "cmd",
+                    Arguments = "/C pause",
+                    UseShellExecute = false
+                });
+            pauseProc.WaitForExit();
         }
 
         static void List(bool autoDelete)
@@ -30,25 +39,24 @@ namespace WifiProfiles
             foreach (var a in profiles)
             {
                 string warning = NetShWrapper.IsOpenAndAutoWifiProfile(a) ? "Warning: AUTO connect to OPEN WiFi" : String.Empty;
-                Console.WriteLine(String.Format("{0,-20} {1,10} {2,10} {3,30} ", a.Name, a.ConnectionMode, a.Authentication, warning));
+                // Console.WriteLine has a "formatting" overload
+                Console.WriteLine("{0,-20} {1,10} {2,10} {3,30} ", a.Name, a.ConnectionMode, a.Authentication, warning);
                 if (!String.IsNullOrWhiteSpace(warning)) sawBadWifi = true;
             }
             if (sawBadWifi)
             {
-                if(!autoDelete) Console.WriteLine("\r\nDelete WiFi profiles that are OPEN *and* AUTO connect? [y/n]");
-                if (autoDelete || Console.ReadLine().Trim().ToUpperInvariant().StartsWith("Y"))
-                {
+                if (!autoDelete) Console.WriteLine("{0}Delete WiFi profiles that are OPEN *and* AUTO connect? [y/n]", Environment.NewLine);
+                if (autoDelete || Console.ReadLine().Trim().ToUpperInvariant().StartsWith("Y")) // Another String.Equals CurrentCultureIgnoreCase?
                     foreach (var a in profiles.Where(a => NetShWrapper.IsOpenAndAutoWifiProfile(a)))
-                    {
                         Console.WriteLine(NetShWrapper.DeleteWifiProfile(a.Name));
-                    }
-                }
             }
             else
             {
-                Console.WriteLine("\r\nNo WiFi profiles set to OPEN and AUTO connect were found. \r\nOption: Run with /deleteautoopen to auto delete.");
+                Console.WriteLine("{0}No WiFi profiles set to OPEN and AUTO connect were found." +
+                                  "{0}Option: Run with /deleteautoopen to auto delete.", Environment.NewLine);
             }
-            //Console.ReadKey();
+
+            if (Environment.UserInteractive) Pause();
         }
     }
 }
